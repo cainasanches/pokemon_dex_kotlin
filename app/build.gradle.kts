c86cda96ffa5aaea82a535c6a55d8405da3d13b1
cainasanches <cainasanches@hotmail.com>
2023-09-19 12:05:33 -0300

    Add core navigation Add Primeiro Layout Login Organização implementations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -67,6 +68,14 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3")
     implementation ("com.github.bumptech.glide:glide:4.13.2")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     //Test Implementation
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/pokemon_dex_2023/ui/theme/feature/feature_viewmodel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.pokemon_dex_2023.ui.theme.feature
-
-class feature_viewmodel {
-}
